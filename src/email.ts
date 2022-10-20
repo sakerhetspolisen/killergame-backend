644576a0843839f1f856fce93cc5a4ed026a3cb2
sakerhetspolisen <68159964+sakerhetspolisen@users.noreply.github.com>
2022-10-20 21:16:12 +0200

    Added Mailgun

--- src/email.ts
+++ src/email.ts
@@ -1,19 +1,26 @@
-import nodemailer from "nodemailer";
-
-const transporter = nodemailer.createTransport({
-  host: "",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
+import Mailgun from "mailgun.js";
+import FormData from "form-data";
+const DOMAIN = "sandbox70b8b52f49924c038ac0745b32265b8e.mailgun.org";
+const mailgun = new Mailgun(FormData);
+const mg = mailgun.client({
+  key: "968e7dc1055c9c1970929a5e4c1ee633-d117dd33-d17d0bb6",
+  username: "api",
 });
 
-transporter.verify(function (error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Server is ready to take our messages");
-  }
-});
+var email = "karlsellergren@protonmail.com";
+var firstName = "Karl";
+var lastName = "Sellergren";
+var id = "637 273";
+
+const data = {
+  from: "Killergame <postmaster@sandbox70b8b52f49924c038ac0745b32265b8e.mailgun.org>",
+  to: email,
+  subject: `${firstName}, här är din inloggningskod.`,
+  template: "player_id",
+  "h:X-Mailgun-Variables": { firstName, playerId: id },
+};
+try {
+  const res = mg.messages.create(DOMAIN, data);
+} catch (err) {
+  console.log(err);
+}
