b3b3cf277c4fc13ebcde2de07b418bd4c4a23ea1
sakerhetspolisen <68159964+sakerhetspolisen@users.noreply.github.com>
2023-11-03 12:36:38 +0100

    Fixed cookie settings

--- src/config/cookieOpts.ts
+++ src/config/cookieOpts.ts
@@ -3,7 +3,8 @@ const env = process.env.NODE_ENV;
 
 export const COOKIE_OPTS: CookieSerializeOptions = {
   path: "/",
-  secure: env === "production",
+  secure: true,
+  httpOnly: true,
   sameSite: "lax",
   signed: true,
   maxAge: 60 * 60 * 24,
